4289a8422100c2733f481849483d0c7fc8eeaa08
isyang1223 <isyang1223@gmail.com>
2017-09-25 20:42:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// write your code below!
+function happyHolidays() {
+  return 'Happy holidays!'
+}
+
+function happyHolidaysTo(Ian) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(Thanksgiving, Ian) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(Christmas, 53) {
+  return `It's ${days} days until ${holiday}!`
+}
